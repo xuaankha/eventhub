3b0353cfb3dfac6b6ea40a97dc9c0d8161283e6a
Xuan Khaa <khaaxuannguyen@gmail.com>
2024-02-22 23:24:49 +0700

    navigator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {SplashScreen} from './src/screens';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
+  const [isShowSplash, setIsShowSplash] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+    return () => clearTimeout(timeout);
+  }, []);
+  return isShowSplash ? (
+    <SplashScreen />
+  ) : (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
   );
 };
 
