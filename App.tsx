5363be82b0c60e25d6cfabb0d0df382188247b73
Xuan Khaa <khaaxuannguyen@gmail.com>
2024-02-23 20:33:24 +0700

    add splashscreen, spacecomponent

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {SplashScreen} from './src/screens';
 import AuthNavigator from './src/navigators/AuthNavigator';
@@ -9,15 +9,24 @@ const App = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsShowSplash(false);
-    }, 1500);
+    }, 1200);
     return () => clearTimeout(timeout);
   }, []);
-  return isShowSplash ? (
-    <SplashScreen />
-  ) : (
-    <NavigationContainer>
-      <AuthNavigator />
-    </NavigationContainer>
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {isShowSplash ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          <AuthNavigator />
+        </NavigationContainer>
+      )}
+    </>
   );
 };
 
