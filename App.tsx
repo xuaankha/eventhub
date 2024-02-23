f937ab222dca3c4b8489cae5ead19b4d441e597a
Xuan Khaa <khaaxuannguyen@gmail.com>
2024-02-23 22:25:05 +0700

    add onboardingscreen, textcpn, authapi

--- App.tsx
+++ App.tsx
@@ -3,15 +3,29 @@ import React, {useEffect, useState} from 'react';
 import {SplashScreen} from './src/screens';
 import AuthNavigator from './src/navigators/AuthNavigator';
 import {NavigationContainer} from '@react-navigation/native';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigators/MainNavigator';
 
 const App = () => {
   const [isShowSplash, setIsShowSplash] = useState(true);
+  const [accessToken, setAccessToken] = useState('');
+  const {getItem, setItem} = useAsyncStorage('assetToken');
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsShowSplash(false);
-    }, 1200);
+    }, 1500);
     return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+  const checkLogin = async () => {
+    const token = await getItem();
+    token && setAccessToken(token);
+  };
+
   return (
     <>
       <StatusBar
@@ -23,7 +37,7 @@ const App = () => {
         <SplashScreen />
       ) : (
         <NavigationContainer>
-          <AuthNavigator />
+          {accessToken ? <MainNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       )}
     </>
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,15 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>LoginScreen</Text>
+      <Button
+        title="Login"
+        onPress={async () => await AsyncStorage.setItem('assetToken', 'fafafa')}
+      />
     </View>
   );
 };
