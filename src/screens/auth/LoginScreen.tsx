7b187d33eb165f717e9e6bd726700765021b922f
Xuan Khaa <khaaxuannguyen@gmail.com>
2024-02-26 21:01:34 +0700

    add icons

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,24 @@
 import {View, Text, Button} from 'react-native';
 import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ButtonComponent} from '../../components';
 
 const LoginScreen = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>LoginScreen</Text>
-      <Button
+      {/* <Button
         title="Login"
         onPress={async () => await AsyncStorage.setItem('assetToken', 'fafafa')}
+      /> */}
+      <ButtonComponent
+        text="LOGIN"
+        onPress={() => console.log('Login')}
+        icon={
+          <View>
+            <Text>N</Text>
+          </View>
+        }
       />
     </View>
   );
